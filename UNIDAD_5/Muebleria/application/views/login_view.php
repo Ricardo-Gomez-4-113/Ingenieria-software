aef76e661cc1a30b6ccf61cec908c6bfd71b0255
Ricardo-Gomez-4-113 <ricardogomez.rg517@gmail.com>
2019-06-13 00:11:28 -0500

    project

--- UNIDAD_5/Muebleria/application/views/login_view.php
+++ UNIDAD_5/Muebleria/application/views/login_view.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("Login/estilo.css");?>"> 
    <title>Login</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <!-- vinculo a bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Temas-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("Login/estilo.css");?>"> 
  </head>
  <body>
-   <h1>Login</h1>
 
      <?php
         if (isset($error_message)) {
@@ -29,8 +28,30 @@
     $attributes = array('id' => 'mylogin');
     echo form_open($route, $attributes);
 
- ?> <label for="email">Username:</label> 
- <input type="text" size="20" id="username" name="username"/> <br/> 
- <label for="pass">Password:</label> 
- <input type="password" size="20" id="password" name="password"/> 
- <br/> <input type="submit" value="Login"/> </form> </body> </html>
+ ?>
+  <div id="Contenedor">
+     <div class="Icon">
+                    <!--Icono de usuario-->
+                   <span class="glyphicon glyphicon-user"></span>
+                 </div>
+<div class="ContentForm">
+<div class="input-group input-group-lg" name="FormEntrar">
+          <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
+          <input type="email" class="form-control" name="username" placeholder="Correo" id="username" aria-describedby="sizing-addon1" required>
+        </div>
+        <br>
+        <div class="input-group input-group-lg">
+          <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
+          <input type="password" name="password" class="form-control" placeholder="******" aria-describedby="sizing-addon1" required>
+        </div>
+        <br>
+        <button class="btn btn-lg btn-primary btn-block btn-signin" id="IngresoLog" type="submit">Entrar</button>
+        </div>
+        </div>
+</form> 
+</body> 
+<!-- vinculando a libreria Jquery-->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ <!-- Libreria java scritp de bootstrap -->
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
